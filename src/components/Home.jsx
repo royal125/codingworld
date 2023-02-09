0807f1d9a8c0bb9f3036a78432a4e5e9ad3dd604
Harinderpal singh <hsbhular@gmail.com>
2023-02-10 00:24:19 +0530

    h

---
+++ src/components/Home.jsx
@@ -0,0 +1,158 @@
+import React from 'react'
+import './Home.css';
+
+
+
+function Home() {
+    return (
+        <>
+        
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <header>
+
+  
+  <div className="overlay"></div>
+
+  <div className='container-fluid vids col-md'>
+  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 padding-zero embed-responsive embed-responsive-16by9">
+  <video  playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+    <source src="videos/design.mp4" type="video/mp4"/>
+  </video>
+  </div>
+  </div>
+ 
+  <div className="container-fluid col-md ">
+    <div className="d-flex h-100 text-center align-items-center">
+      <div className="w-100 tt text-white">
+      <button className="btn bvt  btn-outline-danger" type="submit" value="Submit">Start Tour</button>
+      <button className="btn bvt2 btn-outline-dark " type="submit" value="Submit">Contact Us</button>
+        
+      </div>
+    </div>
+  </div>
+</header>
+
+
+
+            <div className=' sp align-items-center'>
+                <div className="container-fluid flow pt-3 d-md-block">
+                    <div className="row">
+                        <div className='col-10 mx-auto'>
+                            <div className="row">
+                                <div className="col-md-6 ">
+                                    <div className='grow'>
+                                        <h2 className='heda'> Grow your skillset in web development with our useful and free coding resources.  </h2>
+                                        <p className='para'>
+                                            Learn to code while creating real projects
+                                            Engage into getting skills to bring your tech-projects to life!
+                                        </p>
+                                        <a style={{ borderRadius: 100, }} className='btn btn-outline-primary btnn'>Take a tour</a></div>
+
+                                </div>
+
+                                <div className="col-md-6">
+                                    <div className='brand'>
+                                        <img className='img-fluid main-img' src="/images/img-1.png" alt="image" />
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><br/><br/>
+            
+
+
+
+     
+
+
+</div>
+
+
+
+
+<div class="container">
+    <div class="row text-center">
+
+       
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+      
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-2.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+        
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-1.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+       
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-7.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+    		
+
+            <section class="home-newsletter ">
+    <div className='container-fluid nwe '>
+
+<div className="row">
+<div className="col-lg-12">
+	<div className="single">
+		<h2 className='nws'>Subscribe to our Newsletter</h2>
+	<div className="input-group">
+         <input type="email" className="form-control" placeholder="Enter your email"/>
+         <span className="input-group-btn">
+         <button className="btn btn-theme" type="submit">Subscribe</button>
+         </span>
+          </div>
+	</div>
+</div>
+</div>
+</div>
+</section>
+
+        </>
+    )
+}
+
+export default Home;
